158e61e647dad861a819371a38a5ee84444965e1
Alexey <appplemac@gmail.com>
2015-05-14 13:48:30 +0200

    better version of empleats

--- lab5_session/empleats/empleats-opt.c
+++ lab5_session/empleats/empleats-opt.c
@@ -3,26 +3,38 @@
 #include <string.h>
 
 #define MAX_NOM 40
-#define MAX_DIAS_MES 31
-#define MAX_CATEGORIA 40
+#define MAX_DIAS_MES 31 
+#define MAX_CATEGORIA 40 
 #define MAX_ADDRESS 100
-/*#define EXTES 1*/
 
-typedef struct
+#define EXTES 1
+
+typedef struct 
 {
   long long int NID;              /* Número d'identificació: clau d'ordenació */
-  char * Nom;  
-  char * Cognoms;  
-  int  * horesMes;
-  char * Categoria;
+  char Nom[MAX_NOM];  
+  char Cognoms[MAX_NOM];  
+  int  horesMes[MAX_DIAS_MES]; 
+  char Categoria[MAX_CATEGORIA];
   unsigned int ptrClauForaneaDepartament;
   unsigned int ptrClauCategoria;
-#ifdef EXTES
-  char * Address;
-  char * Nom_parella;
-  char * Cognoms_parella;
+#ifdef EXTES     
+  char Address[MAX_ADDRESS];
+  char Nom_parella[MAX_NOM];
+  char Cognoms_parella[MAX_NOM];
 #endif
 } Templeat;
+
+typedef struct {
+  unsigned int NID;
+  unsigned int empleat_index;
+} index_node;
+
+int compare_index(const void *p1, const void *p2) {
+    if (((index_node *) p1)->NID > ((index_node *) p2)->NID) return(1);
+    else if (((index_node *) p1)->NID < ((index_node *) p2)->NID) return(-1);
+    else return(0);
+}
 
 int compare(const void *p1, const void *p2)
 {
@@ -34,52 +46,44 @@ int compare(const void *p1, const void *p2)
 
 int main(int argc, char *argv[])
 {
-  Templeat *empleats;
-  unsigned int N = 4000000, i;
+	Templeat *empleats;
+	unsigned int N = 4000000, i;
 
-  if (argc > 1) N = atoi(argv[1]);
+	if (argc > 1) N = atoi(argv[1]);
 
-  char * addresses = (char *)malloc(MAX_ADDRESS*N);
-  char * couple_names = (char *)malloc(MAX_NOM*N);
-  char * couple_surnames = (char *)malloc(MAX_NOM*N);
+	empleats = (Templeat *) malloc(N*sizeof(Templeat));
+	if (empleats == NULL) { fprintf(stderr, "Out of memory\n"); exit(0); }
+	memset(empleats, 0, N*sizeof(Templeat));
 
-  int * month_hours = (int *)malloc(MAX_DIAS_MES*N*sizeof(int));
-  char * categories = (char *)malloc(MAX_CATEGORIA*N);
+  index_node * index = (index_node *)malloc(sizeof(index_node) * N);
 
-  char * names = (char *)malloc(MAX_NOM*N);
-  char * surnames = (char *)malloc(MAX_NOM*N);
-
-  empleats = (Templeat *) malloc(N*sizeof(Templeat));
-  if (empleats == NULL) { fprintf(stderr, "Out of memory\n"); exit(0); }
-  memset(empleats, 0, N*sizeof(Templeat));
-
-  /* Random generation */
-  for (i=0; i<N; i++) {
+	/* Random generation */
+	for (i=0; i<N; i++) {
 		empleats[i].NID = rand();
-    empleats[i].Nom = &names[i];
-    empleats[i].Cognoms = &surnames[i];
+		index[i].NID = empleats[i].NID;
+    index[i].empleat_index = i;
 		empleats[i].Nom[0] = 'A' + ((i>>3) + (i<<2)) & 0x15;
 		empleats[i].Cognoms[0] = 'a' + ((i>>2) + (i<<3)) & 0x15;
-    empleats[i].horesMes = &month_hours[i];
 		empleats[i].horesMes[i % MAX_DIAS_MES] = i;
-    empleats[i].Categoria = &categories[i];
 		empleats[i].Categoria[i % MAX_CATEGORIA] = (i << 2) * i;
 		empleats[i].ptrClauForaneaDepartament = i<<10 + i<<14 + i>>3;
 		empleats[i].ptrClauCategoria = i<<24 + i<< 18 + i<<10 + i>>2;
-#ifdef EXTES
-                empleats[i].Address = &addresses[i];
-                empleats[i].Nom_parella = &couple_names[i];
-                empleats[i].Cognoms_parella = &couple_names[i];
+#ifdef EXTES     
                 empleats[i].Address[0] = 'A' + ((i>>4) + (i<<2)) & 0x15;
                 empleats[i].Nom_parella[0] = 'a' + ((i>>5) + (i<<2)) & 0x15;
-      empleats[i].Cognoms_parella[0] = 'A' + ((i>>5) + (i<<3)) & 0x15;
+  		empleats[i].Cognoms_parella[0] = 'A' + ((i>>5) + (i<<3)) & 0x15;
 #endif
-  }
+	}
 
-  qsort(empleats, N, sizeof(Templeat), compare);
-  for (i=0; i<N; i+=2000){
-          write(1, &empleats[i],2000*sizeof(Templeat));
-  }
+	qsort(index, N, sizeof(index_node), compare_index);
+  Templeat * buffer = (Templeat *)malloc(sizeof(Templeat)*2000);
+  int j;
+	for (i=0; i<N; i+=2000){
+    for (j=0; j<2000 && i+j<N; ++j) {
+      buffer[j] = empleats[index[i+j].empleat_index];
+    }
+    write(1, buffer,(sizeof(Templeat)*j));
+	}
 
-  return 0;
+	return 0;
 }
